1893c461d14447a80a779a34b482262f59e87c27
AshishAashu <ashishkaushalraj110@gmail.com>
2018-03-20 12:30:40 +0530

    index title changed

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html>
 	<head>
 		<title>
-			ClientSearch				
+			ClientSearch tag				
 		</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/b>
   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
